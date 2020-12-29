8ca50b18ec5f5241f4332dfab24431a237f2d233
irakli <mamulashviliirakli12@gmail.com>
2020-12-29 20:14:15 +0400

    add hover and check

--- src/test/java/finalTest.java
+++ src/test/java/finalTest.java
@@ -1,14 +1,11 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
+
 
 @Test
 public class finalTest {
@@ -20,12 +17,51 @@ public class finalTest {
         driver.get("http://automationpractice.com/index.php");
 
         //select T-shirt
-        WebElement T_shirts = driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li/ul/li/ul/li/a"));
+        WebElement T_shirts = driver.findElement(By.cssSelector("ul.submenu-container a[title='T-shirts']"));
         js.executeScript("arguments[0].click();",T_shirts);
         Actions action = new Actions(driver);
         js.executeScript("window.scrollBy(0,500)");
         WebElement Quick = driver.findElement(By.cssSelector("[itemprop = 'image']"));
         action.moveToElement(Quick).perform();
+        driver.findElement(By.className("quick-view")).click();
+
+        //wait and switchTo frame
+        new WebDriverWait(driver, 300).ignoring(StaleElementReferenceException.class)
+                .ignoring(WebDriverException.class)
+                .until(ExpectedConditions.visibilityOf((driver.findElement(By.className("fancybox-iframe")))));
+        driver.switchTo().frame(driver.findElement(By.className("fancybox-iframe")));
+
+        //hover and check
+        WebElement src_1 = driver.findElement(By.id("bigpic"));
+        src_1.getAttribute("src");
+
+        WebElement image_1 = driver.findElement(By.id("thumb_1"));
+        WebElement image_2 = driver.findElement(By.id("thumb_2"));
+        WebElement image_3 = driver.findElement(By.id("thumb_3"));
+        WebElement image_4 = driver.findElement(By.id("thumb_4"));
+
+        action.moveToElement(image_1).moveToElement(image_2).moveToElement(image_3)
+                .moveToElement(image_4).perform();
+        WebElement src_2 = driver.findElement(By.id("bigpic"));
+        src_2.getAttribute("src");
+
+        if (src_1 != src_2){
+            System.out.println("image is changed");
+        }else {
+            System.out.println("image is not change");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
